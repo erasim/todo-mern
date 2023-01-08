89da810a71ff4752ad3c75f4ebebc1132e8617c8
erasim.zaidi <erasim.zaidi@gmail.com>
2023-01-09 03:00:25 +0530

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
-const {JWT_SECRET,MOGOURI} = require('./config/keys')
+
 const dbo = require("./db/conn");
 if(process.env.NODE_ENV=='production'){
   const path = require('path')
