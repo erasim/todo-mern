928730c0f45f129dcdd278aa388a07cac84a3b41
erasim.zaidi <erasim.zaidi@gmail.com>
2023-01-15 00:49:26 +0530

    update

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -28,7 +28,7 @@ export default function RecordList() {
  // This method fetches the records from the database.
  useEffect(() => {
    async function getRecords() {
-     const response = await fetch(`./record/`);
+     const response = await fetch(url+`/record/`);
  
      if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
